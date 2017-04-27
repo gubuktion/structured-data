aae83eba2b89a2989a462b15d51ec43959ddee73
twoplusbeef <gubuktion@gmail.com>
2017-04-27 22:07:15 +0300

    Implement <conrains-rectangle?> function

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -56,7 +56,12 @@
           :else false)))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[[x1 y1] [x2 y2]] outer [[x3 y3] [x4 y4]] inner]
+    (cond (and (<= x1 x3 x4 x2) (<= y1 y3 y4 y2)) true
+          (and (<= x1 x4 x3 x2) (<= y1 y4 y3 y2)) true
+          (and (<= x2 x3 x4 x1) (<= y2 y3 y4 y1)) true
+          (and (<= x2 x4 x3 x1) (<= y2 y3 y4 y1)) true
+          :else false)))
 
 (defn title-length [book]
   :-)
